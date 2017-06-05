0591e3e8eebc0d9ff46209f4cf43f51cccc996a5
Kent Mewhort <kent@openissues.ca>
2017-06-05 15:23:47 -0700

    Completed scan of Gem's in productio db.

--- db/migrate/20170527191231_add_allow_unknown_libraries_to_policy.rb
+++ db/migrate/20170527191231_add_allow_unknown_libraries_to_policy.rb
@@ -1,5 +1,5 @@
 class AddAllowUnknownLibrariesToPolicy < ActiveRecord::Migration
   def change
-    add_column :policies, :allow_unknown_libraries, :boolean, default: false
+    add_column :policies, :allow_unknown_libraries, :boolean, default: true
   end
 end
