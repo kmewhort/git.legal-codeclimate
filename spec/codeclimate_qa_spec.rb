a56711d64628ea17f9b5e71277928b35bde43963
Kent Mewhort <kent@openissues.ca>
2017-07-04 20:56:01 -0700

    Add QA specs for python libs.

--- spec/codeclimate_qa_spec.rb
+++ spec/codeclimate_qa_spec.rb
@@ -6,59 +6,83 @@ describe 'Codeclimate QA' do
   let(:tmp_download_dir) { Dir.mktmpdir }
 
   describe 'Popular repos' do
-    repos = [
+    repos = {
       # top ten Github ruby repos, by stars, based on the following query (with non-ruby repos then discarded):
       # https://github.com/search?l=Ruby&o=desc&q=stars:>1&ref=searchresults&s=stars&type=Repositories&utf8=✓
-      'rails/rails',
-      'jekyll/jekyll',
-      'discourse/discourse',
-      'gitlabhq/gitlabhq',
-      'plataformatec/devise',
-      'fastlane/fastlane',
-      'huginn/huginn',
-      'mitchellh/vagrant',
-      'Thibaut/devdocs',
-      'jondot/awesome-react-native',
+      ruby: [
+        'rails/rails',
+        'jekyll/jekyll',
+        'discourse/discourse',
+        'gitlabhq/gitlabhq',
+        'plataformatec/devise',
+        'fastlane/fastlane',
+        'huginn/huginn',
+        'mitchellh/vagrant',
+        'Thibaut/devdocs',
+        'jondot/awesome-react-native'
+      ],
 
       # js/npm - https://github.com/search?l=JavaScript&o=desc&q=stars:>1&ref=searchresults&s=stars&type=Repositories&utf8=✓
-      'freeCodeCamp/freeCodeCamp',
-      'twbs/bootstrap',
-      'facebook/react',
-      'd3/d3',
-      'vuejs/vue'
-    ]
+      js: [
+        'freeCodeCamp/freeCodeCamp',
+        'twbs/bootstrap',
+        'facebook/react',
+        'd3/d3',
+        'vuejs/vue'
+      ],
 
-    repos.each do |repo_name|
-      describe repo_name do
-        before do
-          git_url = "git@github.com:#{repo_name}"
-          pull_cmd = "cd #{tmp_download_dir} && git init && git pull --depth=1 #{git_url} master"
-          system(pull_cmd) or raise "Unable to pull remote repository ('#{pull_cmd}' failed)"
+      # python - https://github.com/search?l=Python&o=desc&q=stars:>1&ref=searchresults&s=stars&type=Repositories&utf8=✓
+      # --but redacted to only ones with a checked-in requirements.txt
+      python: [
+        'requests/requests',
+        'ansible/ansible:devel',
+        'scrapy/scrapy'
+      ]
 
-          # docker can't connect to /var/folders/ on OSX, so use the project tmpdir; however, for git, need to be
-          # outside project
-          orig_tmp_dir = ENV['TMPDIR']
-          ENV['TMPDIR'] = "#{File.dirname(__FILE__)}/../tmp"
-          @tmp_code_dir = Dir.mktmpdir
-          ENV['TMPDIR'] = orig_tmp_dir
-          `mv #{tmp_download_dir}/* #{@tmp_code_dir}/`
+    }
 
-          IO.write "#{@tmp_code_dir}/.codeclimate.yml", <<~YML
-          ---
-          engines:
-            git-legal:
-              enabled: true
-          YML
-        end
+    repos.each do |type, repos|
+      describe type do
+        repos.each do |repo_name_and_branch|
+          describe repo_name_and_branch do
+            before do
+              if repo_name_and_branch.include? ':'
+                repo_name, branch = repo_name_and_branch.split ':'
+              else
+                repo_name = repo_name_and_branch
+                branch = 'master'
+              end
 
-        it "executes successfully" do
-          result = `cd #{@tmp_code_dir} && codeclimate analyze --dev -e git-legal`
+              git_url = "git@github.com:#{repo_name}"
+              pull_cmd = "cd #{tmp_download_dir} && git init && git pull --depth=1 #{git_url} #{branch}"
+              system(pull_cmd) or raise "Unable to pull remote repository ('#{pull_cmd}' failed)"
 
-          puts "------ Result for #{repo_name} ------"
-          puts result
-          puts "-------------------------------------"
+              # docker can't connect to /var/folders/ on OSX, so use the project tmpdir; however, for git, need to be
+              # outside project
+              orig_tmp_dir = ENV['TMPDIR']
+              ENV['TMPDIR'] = "#{File.dirname(__FILE__)}/../tmp"
+              @tmp_code_dir = Dir.mktmpdir
+              ENV['TMPDIR'] = orig_tmp_dir
+              `mv #{tmp_download_dir}/* #{@tmp_code_dir}/`
 
-          expect(result).to include 'Analysis complete!'
+              IO.write "#{@tmp_code_dir}/.codeclimate.yml", <<~YML
+              ---
+              engines:
+                git-legal:
+                  enabled: true
+              YML
+            end
+
+            it "executes successfully" do
+              result = `cd #{@tmp_code_dir} && codeclimate analyze --dev -e git-legal`
+
+              puts "------ Result for #{repo_name_and_branch} ------"
+              puts result
+              puts "-------------------------------------"
+
+              expect(result).to include 'Analysis complete!'
+            end
+          end
         end
       end
     end
